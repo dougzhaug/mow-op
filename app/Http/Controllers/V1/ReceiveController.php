5fa208ea42f99c3ecf6e74d5dd7d5b72a5906e59
homeMac <homeMac@qq.com>
2018-04-14 18:46:47 +0800

    测试提交

--- app/Http/Controllers/V1/ReceiveController.php
+++ app/Http/Controllers/V1/ReceiveController.php
@@ -41,6 +41,15 @@ class ReceiveController extends Controller
 
     public function bindCallback()
     {
+        $config = [
+            'app_id'   => 'wx283587b496831502',//'开放平台第三方平台 APPID',
+            'secret'   => 'd71efa5d2ec2d0b90544419df3b6485e',//'开放平台第三方平台 Secret',
+            'token'    => 'openspen',//'开放平台第三方平台 Token',
+            'aes_key'  => '04426065eb6795c0b414d282d52f03f6openspenvip',//'开放平台第三方平台 AES Key'
+        ];
 
+        $this->openPlatform = Factory::openPlatform($config);
+        $a = $this->openPlatform->handleAuthorize();
+        dd($a);
     }
 }
